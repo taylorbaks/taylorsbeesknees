37e3a6915387ab0615e7b86b497996a8ae043422
taylorbaks <149135365+taylorbaks@users.noreply.github.com>
2023-11-07 09:56:03 -0500

    better java and whatnot

--- index.js
+++ index.js
@@ -200,17 +200,16 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
     const slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    slides[slideIndex].style.display = "block";
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 10000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds (matching the animation duration)
 }
 
 function toggleParagraph() {
